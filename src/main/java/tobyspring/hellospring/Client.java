e941b091292a05ff3fdba0e8ec61a6a8481a5621
jyp-on <okmlnsunok@naver.com>
2025-01-22 12:04:22 +0900

    오브젝트 팩토리

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -5,7 +5,8 @@ import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService(new SimpleExRateProvider());
+        ObjectFactory objectFactory = new ObjectFactory();
+        PaymentService paymentService = objectFactory.paymentService();
         Payment prepare = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(prepare);
     }
