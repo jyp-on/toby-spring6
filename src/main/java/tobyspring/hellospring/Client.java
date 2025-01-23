e9877e31d5a51f4767369e3a306b090357e56c59
jyp-on <okmlnsunok@naver.com>
2025-01-23 11:16:52 +0900

    스프링 컨테이너와 의존관계 주입

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -2,11 +2,13 @@ package tobyspring.hellospring;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        ObjectFactory objectFactory = new ObjectFactory();
-        PaymentService paymentService = objectFactory.paymentService();
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
         Payment prepare = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(prepare);
     }
